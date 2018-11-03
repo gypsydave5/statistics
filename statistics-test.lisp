0ec803ca84c28e96cbce4260bbd77269376fd7ca
David Wickes <dave.wickes@gmail.com>
2018-11-03 23:51:45 +0000

    Beginning to write tests

---
+++ statistics-test.lisp
@@ -0,0 +1,38 @@
+(ql:quickload "lisp-unit")
+(load "statistics")
+
+(defpackage :com.gypsydave5.statistics-test
+  (:use #:common-lisp #:lisp-unit #:com.gypsydave5.statistics))
+
+(in-package :com.gypsydave5.statistics-test)
+
+(defparameter *odd-length-numbers*
+  (list 3 3 6 7 7 10 10 10 11 13 30))
+
+(defparameter *even-length-numbers*
+  (list 1 2 3 4 5 6))
+
+(define-test mean-test
+  (loop for (expected population)
+     in `((7/2 ,*even-length-numbers*)
+          (10 ,*odd-length-numbers*)
+          (4 (3 4 5)))
+     do (assert-equal expected (mean population) population)))
+
+(define-test median-test
+  (assert-equal 7/2 (median *even-length-numbers*))
+  (assert-equal 10 (median *odd-length-numbers*))
+  (assert-equal 3 (median (list 5 3 2 4 1)))
+  (assert-equal 3.0 (median (list 1.0 2.0 4.0 5.0))))
+
+(define-test mode-test
+  (assert-equality #'set-equal *even-length-numbers* (mode *even-length-numbers*))
+  (assert-equal (list 10) (mode *odd-length-numbers*))
+  (assert-equality #'set-equal (list 1 3) (mode (list 1 3 1 3 1 3 1 2 3 4))))
+
+(defun run-statistics-tests ()
+  (let ((lisp-unit:*print-errors* t)
+        (lisp-unit:*print-failures* t))
+    (run-tests)))
+
+(run-statistics-tests)
--- statistics.lisp
+++ statistics.lisp
@@ -1,14 +1,16 @@
+(defpackage :com.gypsydave5.statistics
+  (:use #:common-lisp)
+  (:export #:mean #:mode #:median))
+
+(in-package #:com.gypsydave5.statistics)
+
 ;; mean
-(defun mean (&rest ns)
+(defun mean (ns)
   "Calculates the mean of numbers NS"
   (when (null ns) (error "Cannot have a mean of no values"))
   (/ (apply #'+ ns) (length ns)))
 
-(mean 3 4 5)
-;; => 4
-
-
-(defun median (&rest ns)
+(defun median (ns)
   "Calculates the median of numbers NS and the index of the median"
   (let* ((ns (sort ns #'<))
          (l (length ns))
@@ -16,16 +18,16 @@
     (cond
       ((null ns) (error "Cannot have a median value of no values"))
       ((evenp l)
-       (mean (nth (1- mid) ns)
-             (nth mid ns)))
+       (mean (list (nth (1- mid) ns)
+                   (nth mid ns))))
       (t (nth (floor mid) ns)))))
 
-(median 5 1 3 2 4)
+(median (list 5 1 3 2 4))
 ;; => 3
-(median 1.0 2.0 4.0 5.0)
+(median (list 1.0 2.0 4.0 5.0))
 ;; => 3.0
 
-(defun mode (&rest xs)
+(defun mode (xs)
   "Calculates the modes of XS and returns them as well as the count."
   (let ((counts (make-hash-table :test 'equal))
         (max 0)
@@ -41,11 +43,11 @@
                                              (setf modes (list value)))))) counts)
     (values modes max)))
 
-(mode 1 3 1 3 1 3 1 2 3 4)
+
 ;; => (3 1)
 ;;    4
 
-(mode 'a 'b 'c 'd 'e 'a)
+(mode (list'a 'b 'c 'd 'e 'a))
 ;; => (A)
 ;;    2
 
@@ -59,46 +61,6 @@
 (range 1 3 10 8)
 ;; => 9
 
-(defun lower-quartile (&rest ns)
-  (let ((sorted-ns (sort ns #'<)))
-    (apply #'median (subseq sorted-ns
-                            0
-                            (ceiling (/ (1- (length sorted-ns)) 2))))))
-
-(defun upper-quartile (&rest ns)
-  (let ((sorted-ns (sort ns #'<)))
-    (apply #'median (subseq sorted-ns
-                            (ceiling (/ (length sorted-ns) 2))))))
-
-(defparameter *odd-length-numbers*
-  (list 3 3 6 7 7 10 10 10 11 13 30))
-
-(defparameter *even-length-numbers*
-  (list 1 2 3 4 5 6))
-
-(apply #'lower-quartile *even-length-numbers*)
-;; => 2
-
-(apply #'upper-quartile *even-length-numbers*)
-;; => 5
-
-(apply #'lower-quartile *odd-length-numbers*)
-;; => 6
-
-(apply #'upper-quartile *odd-length-numbers*)
-;; => 11
-
-(defun quartiles (&rest ns)
-  (list (apply #'lower-quartile ns)
-        (apply #'median ns)
-        (apply #'upper-quartile ns)))
-
-(apply #'quartiles *even-length-numbers*)
-;; => (2 7/2 5)
-
-(apply #'quartiles *odd-length-numbers*)
-;; => (6 10 11)
-
 (defun quantiles (q ns)
   (let ((sorted-ns (sort ns #'<)))
     (loop for k from 1 to (1- q)
@@ -107,10 +69,29 @@
 (defun quantile (k q sorted-ns)
   (let ((x (* k (/ (length sorted-ns) q))))
         (if (integerp x)
-            (mean (nth (1- x) sorted-ns)
-                  (nth x sorted-ns))
+            (mean (list (nth (1- x) sorted-ns)
+                        (nth x sorted-ns)))
             (nth (floor x) sorted-ns))))
 
-(defun quartiles (&rest ns)
+(defun quartiles (ns)
   (quantiles 4 ns))
-;; => QUARTILES
+
+(defparameter *odd-length-numbers*
+  (list 3 3 6 7 7 10 10 10 11 13 30))
+
+(defparameter *even-length-numbers*
+  (list 1 2 3 4 5 6))
+
+(quartiles *even-length-numbers*)
+;; => (2 7/2 5)
+
+(quartiles *odd-length-numbers*)
+;; => (6 10 11)
+
+(defun interquartile-range (&rest ns)
+  (destructuring-bind (q1 _ q3) (quartiles ns)
+    (declare (ignore _))
+    (- q3 q1)))
+
+(apply #'interquartile-range *odd-length-numbers*)
+;; => 5
